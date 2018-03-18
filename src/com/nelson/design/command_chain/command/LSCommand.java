3a1557c51d75b7ff3d5e6500b184437d153e1308
wanghaoxun <wanghaoxun@guazi.com>
2018-03-18 23:03:52 +0800

    Add code design pattern of command_chain

---
+++ src/com/nelson/design/command_chain/command/LSCommand.java
@@ -0,0 +1,19 @@
+package com.nelson.design.command_chain.command;
+
+
+import com.nelson.design.command_chain.CommandVO;
+import com.nelson.design.command_chain.command_name.CommandName;
+import com.nelson.design.command_chain.command_name.ls.AbstractLS;
+
+/**
+ * @author cbf4Life cbf4life@126.com
+ * I'm glad to share my knowledge with you all.
+ */
+public class LSCommand extends Command {
+
+    public String execute(CommandVO vo) {
+        //返回链表的首节点
+        CommandName firstNode = super.buildChain(AbstractLS.class).get(0);
+        return firstNode.handleMessage(vo);
+    }
+}
---
+++ src/com/nelson/design/command_chain/command_name/df/DF.java
@@ -0,0 +1,20 @@
+package com.nelson.design.command_chain.command_name.df;
+
+import com.nelson.design.command_chain.CommandVO;
+import com.nelson.design.command_chain.command_name.os.DiskManager;
+
+/**
+ * @author cbf4Life cbf4life@126.com
+ * I'm glad to share my knowledge with you all.
+ */
+public class DF extends AbstractDF {
+    //定义一下自己能处理什么参数
+    protected String getOperateParam() {
+        return super.DEFAULT_PARAM;
+    }
+
+    //命令处理
+    protected String echo(CommandVO vo) {
+        return DiskManager.df();
+    }
+}
