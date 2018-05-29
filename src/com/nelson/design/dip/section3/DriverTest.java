4d35aef00d1821ffdb56a152a6001c58496cba4d
wanghaoxun <wanghaoxun@guazi.com>
2018-05-29 23:58:26 +0800

    Add code design pattern of dip

---
+++ src/com/nelson/design/dip/section3/DriverTest.java
@@ -0,0 +1,24 @@
+package com.nelson.design.dip.section3;
+
+import junit.framework.TestCase;
+
+/**
+ * @author cbf4Life cbf4life@126.com
+ * I'm glad to share my knowledge with you all.
+ * 测试类
+ */
+public class DriverTest extends TestCase {
+//    Mockery context = new JUnit4Mockery();
+//
+//    @Test
+//    public void testDriver() {
+//        //根据接口虚拟一个对象
+//        final ICar car = context.mock(ICar.class);
+//        IDriver driver = new Driver();
+//        context.checking(new Expectations() {{
+//            oneOf(car).run();
+//        }});
+//        driver.drive(car);
+//    }
+
+}
