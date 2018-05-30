82f1c3a041775f2675816cbdfca8de37661febce
wanghaoxun <wanghaoxun@guazi.com>
2018-05-30 22:55:39 +0800

    Add code design pattern of isp

---
+++ src/com/nelson/design/isp/section2/IGreatTemperamentGirl.java
@@ -0,0 +1,11 @@
+package com.nelson.design.isp.section2;
+
+/**
+ * @author cbf4Life cbf4life@126.com
+ * I'm glad to share my knowledge with you all.
+ * 气质定义
+ */
+public interface IGreatTemperamentGirl {
+    //要有气质
+    public void greatTemperament();
+}
